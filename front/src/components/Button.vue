6074dc6fd144ce38ba12d27db762cd965b226b27
Laurent <58115082+naaturel@users.noreply.github.com>
2025-03-17 10:51:32 +0100

    They missed their flight...

---
+++ front/src/components/Button.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+  const props = defineProps<{
+    description: string;
+  }>();
+
+</script>
+
+<template>
+  <div class="button">{{ description }}</div>
+</template>
+
+<style scoped>
+
+.button {
+  color: var(--primary-color);
+  text-align: center;
+  align-content: center;
+
+  border-radius: var(--radius);
+  background-color: var(--secondary-color);
+  word-break: break-word;
+  min-height: 7vh;
+}
+
+.button:hover
+{
+  transform: scale(1.1);
+  background-color: var(--header-color);
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+}
+
+</style>
---
+++ front/src/components/InputField.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="input-field">
+    <input/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
